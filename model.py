7d4878273f3a8479d585fa8125d94e71819e555d
James Oswald <jtoswald@albany.edu>
2021-06-05 02:39:11 -0400

    repathed dataset

--- model.py
+++ model.py
@@ -1,8 +1,8 @@
 
 import tensorflow as tf
 
-tf.data.experimental.CsvDataset(
+dataset = tf.data.experimental.CsvDataset(
     filenames, record_defaults, compression_type=None, buffer_size=None,
-    header=False, field_delim=',', use_quote_delim=True,
+    header=False, field_delim='\t', use_quote_delim=True,
     na_value='', select_cols=None, exclude_cols=None
 )
